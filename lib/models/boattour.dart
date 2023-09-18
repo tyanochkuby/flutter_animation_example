f4d9cc59f9bf977f18b0a7fecd94f11fba9482c6
tyanochku_by <ramyud@st.amu.edu.pl>
2023-09-19 00:25:42 +0300

    title animation added, bouttour class moved to separated file

---
+++ lib/models/boattour.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class BoatTour {
+  BoatTour(
+      {required this.title,
+      required this.boatAssetName,
+      required this.cardColor});
+
+  String title;
+  String boatAssetName;
+  Color cardColor;
+}
