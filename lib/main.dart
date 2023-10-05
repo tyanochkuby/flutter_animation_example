1c60ddd1b85ccf8133545bd68d3f7f4491811eae
tyanochku_by <ramyud@st.amu.edu.pl>
2023-10-05 10:57:47 +0200

    firebase auth init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animations_example/presentation/homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
